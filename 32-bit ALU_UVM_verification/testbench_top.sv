03450a0e3378d8234a8abe9fc20005bb197c7a69
Mujtaba <111755551+Mujtabadar537@users.noreply.github.com>
2024-01-09 12:44:47 +0500

    Update testbench_top.sv

--- 32-bit ALU_UVM_verification/testbench_top.sv
+++ 32-bit ALU_UVM_verification/testbench_top.sv
@@ -1,1 +1,38 @@
+module testbench_top;
 
+import uvm_pkg::*;
+import my_uvm_package::*;
+
+
+alu_if _if();
+
+
+ALU arithmetic_logic_unit(
+
+.in1(_if.in1),
+.in2(_if.in2),
+.ALUControl(_if.ALUControl),
+
+.ALUResult(_if.ALUResult),
+.zero(_if.zero)
+
+);
+
+
+
+initial begin
+uvm_config_db#(virtual alu_if)::set(null,"*","virtual_interface",_if);
+run_test("test1");
+end
+
+
+
+
+
+
+
+
+
+
+
+endmodule
